e3ae6ffcfa8aa3807cc83083ce865723203f972f
Akram Hossain <nayanbd17@gmail.com>
2025-02-11 13:31:34 +0600

    all done

--- app/Livewire/Todo.php
+++ app/Livewire/Todo.php
@@ -15,6 +15,11 @@ class Todo extends Component
 
     public $search;
 
+    public $editingId;
+
+    #[Rule('required|min:5|max:50')]
+    public $editingName;
+
     public function create()
     { 
         $validated = $this->validateOnly('name');
@@ -24,6 +29,42 @@ class Todo extends Component
         $this->reset('name');
 
         session()->flash('success', 'Todo created successfully');
+
+        $this->resetPage();
+    }
+
+    public function delete(ModelsTodo $todo)
+    {
+        $todo->delete();
+        session()->flash('success', 'Todo deleted successfully');
+    }
+
+    public function toggle(ModelsTodo $todo)
+    {
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
+
+    public function edit(ModelsTodo $todo)
+    {
+        $this->editingId = $todo->id; 
+        $this->editingName = $todo->name;
+    }
+
+    public function update()
+    {
+        $this->validateOnly('editingName');
+
+        ModelsTodo::find($this->editingId)->update([
+            'name' => $this->editingName
+        ]);
+
+        $this->cancel();
+    }
+
+    public function cancel()
+    {
+        $this->reset('editingId','editingName');
     }
 
     public function render()
